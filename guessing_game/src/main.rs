d500eb6fee29258def4f826a4aff14c60b0f936a
John Soo <jsoo1@asu.edu>
2019-09-15 23:37:43 -0700

    Random stuff.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,10 +1,21 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess, my guy.");
+    let secret = rand::thread_rng().gen_range(0, 100);
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect(
         "Failed to read guess.",
     );
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win!"),
+    };
+
+    println!("Number was {}", secret);
 }
