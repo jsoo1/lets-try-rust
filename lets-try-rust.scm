7148771cbf7f36c9fd37343475a0ced674a22c74
John Soo <jsoo1@asu.edu>
2019-09-15 22:09:54 -0700

    Start guessing game.

--- lets-try-rust.scm
+++ lets-try-rust.scm
@@ -4,9 +4,9 @@
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages))
 
-(define-public lets-try-rust
+(define-public lets-try-rust-hello-world
   (package
-    (name "lets-try-rust")
+    (name "lets-try-rust-hello-world")
     (version "0.0.0")
     (source
      (origin
@@ -20,8 +20,11 @@
          "0iqrg9p23s4xz14aqb47116zq94654kr5gq7ljqiv61h9s1i0hwa"))))
     (build-system cargo-build-system)
     (arguments
-     `(#:cargo-inputs ()))
+     `(#:phases
+       (modify-phases %standard-phases
+         (add-after 'unpack 'chdir-hello-world
+           (lambda _ (chdir "hello_world") #t)))))
     (home-page "https://github.com/jsoo1/lets-try-rust")
-    (synopsis "The Rust Book tm")
-    (description "Work throught the Rust Book")
+    (synopsis "Rust Book Hello World")
+    (description "Work throught the Rust Book. Hello World!")
     (license #f)))
