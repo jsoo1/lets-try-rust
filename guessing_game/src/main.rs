23250feee489f944ed8cc76737e2947a70147aea
John Soo <jsoo1@asu.edu>
2019-09-16 09:01:02 -0700

    Use a loop.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,18 +3,25 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Guess, my guy.");
     let secret = rand::thread_rng().gen_range(0, 100);
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect(
-        "Failed to read guess.",
-    );
-    let guess: u32 = guess.trim().parse().expect("Please enter a number");
-    match guess.cmp(&secret) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win!"),
-    };
 
-    println!("Number was {}", secret);
+    loop {
+        println!("Guess, my guy.");
+
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect(
+            "Failed to read guess.",
+        );
+
+        let guess: u32 = guess.trim().parse().expect("Please enter a number");
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        };
+    }
 }
